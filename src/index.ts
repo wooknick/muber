cc161cd066142c895c8411950cf65b11b293f6e7
wooknick <wooknick@naver.com>
2019-10-30 18:45:57 +0900

    setup dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Options } from "graphql-yoga";
 import { createConnection } from "typeorm";
 import app from "./app";
+import "./env";
 import connectionOptions from "./ormConfig";
 
 const PORT: number | string = process.env.PORT || 4000;
@@ -17,6 +18,8 @@ const handleAppStart = () => {
     console.log(`Listening on port ${PORT}`);
 };
 
-createConnection(connectionOptions).then(() => {
-    app.start(appOptions, handleAppStart);
-});
+createConnection(connectionOptions)
+    .then(() => {
+        app.start(appOptions, handleAppStart);
+    })
+    .catch(error => console.log(error));
