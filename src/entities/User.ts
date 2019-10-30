29d505f7602755a5738b4a3c370dd1fe61be227e
wooknick <wooknick@naver.com>
2019-10-30 21:20:58 +0900

    add bcrypt for 'user' entity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,12 +1,17 @@
+import bcrypt from "bcrypt";
 import { IsEmail } from "class-validator";
 import {
     BaseEntity,
+    BeforeInsert,
+    BeforeUpdate,
     Column,
     CreateDateColumn,
     Entity,
     PrimaryGeneratedColumn,
     UpdateDateColumn
 } from "typeorm";
+
+const BCRYPT_ROUNDS = 10;
 
 @Entity()
 class User extends BaseEntity {
@@ -59,15 +64,32 @@ class User extends BaseEntity {
     @Column({ type: "double precision", default: 0 })
     lastOrientation: number;
 
-    get fullName(): string {
-        return `${this.firstName} ${this.lastName}`;
-    }
-
     @CreateDateColumn()
     createdAt: string;
 
     @UpdateDateColumn()
     updatedAt: string;
+
+    get fullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    public comparePassword(password: string): Promise<boolean> {
+        return bcrypt.compare(password, this.password);
+    }
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    async savePassword(): Promise<void> {
+        if (this.password) {
+            const hashedPassword = await this.hashPassword(this.password);
+            this.password = hashedPassword;
+        }
+    }
+
+    private hashPassword(password: string): Promise<string> {
+        return bcrypt.hash(password, BCRYPT_ROUNDS);
+    }
 }
 
 export default User;
