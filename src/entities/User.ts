a2e88cc36e1552ccd31de5951daa160323b97950
wooknick <wooknick@naver.com>
2019-11-06 23:56:18 +0900

    fix user/verification entity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -15,7 +15,6 @@ import {
 import Chat from "./Chat";
 import Message from "./Message";
 import Ride from "./Ride";
-import Verification from "./Verification";
 
 const BCRYPT_ROUNDS = 10;
 
@@ -78,9 +77,6 @@ class User extends BaseEntity {
 
     @OneToMany(type => Message, message => message.user)
     messages: Message[];
-
-    @OneToMany(type => Verification, verification => verification.user)
-    verifications: Verification[];
 
     @OneToMany(type => Ride, ride => ride.passenger)
     ridesAsPassenger: Ride[];
--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -4,12 +4,10 @@ import {
     Column,
     CreateDateColumn,
     Entity,
-    ManyToOne,
     PrimaryGeneratedColumn,
     UpdateDateColumn
     } from "typeorm";
 import { verificationTarget } from "../types/types";
-import User from "./User";
 
 const PHONE = "PHONE";
 const EMAIL = "EMAIL";
@@ -27,12 +25,6 @@ class Verification extends BaseEntity {
 
     @Column({ type: "text" })
     key: string;
-
-    @Column({ type: "boolean", default: false })
-    used: boolean;
-
-    @ManyToOne(type => User, user => user.verifications)
-    user: User;
 
     @CreateDateColumn()
     createdAt: string;
