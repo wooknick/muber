7a2f9b7bcd6c121091c6219b9996ed697a0d2fce
wooknick <wooknick@naver.com>
2019-12-29 04:32:03 +0900

    Revert "add verify user"
    This reverts commit 26e731cf155957b1b056c6564daadd08e6134432.

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,15 @@
 import cors from "cors";
-import { NextFunction, Response } from "express";
 import { GraphQLServer } from "graphql-yoga";
 import helmet from "helmet";
 import logger from "morgan";
 import "./env";
 import schema from "./schema";
-import decodeJWT from "./utils/decodeJWT";
 
 class App {
     public app: GraphQLServer;
     constructor() {
         this.app = new GraphQLServer({
-            schema,
-            context: req => {
-                return {
-                    req: req.request
-                };
-            }
+            schema
         });
         this.middlewares();
     }
@@ -24,24 +17,6 @@ class App {
         this.app.express.use(cors());
         this.app.express.use(logger("dev"));
         this.app.express.use(helmet());
-        this.app.express.use(this.jwt);
-    };
-
-    private jwt = async (
-        req,
-        res: Response,
-        next: NextFunction
-    ): Promise<void> => {
-        const token = req.get("X-JWT");
-        if (token) {
-            const user = await decodeJWT(token);
-            if (user) {
-                req.user = user;
-            } else {
-                req.user = undefined;
-            }
-        }
-        next();
     };
 }
 
