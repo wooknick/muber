c0c175ccb22dedd6ef9971e3687d6c2a5aa72198
wooknick <wooknick@naver.com>
2020-01-03 00:00:47 +0900

    fix updateMyProfile

--- src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
+++ src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
@@ -1,6 +1,7 @@
 import User from "../../../entities/User";
 import { UpdateMyProfileMutationArgs, UpdateMyProfileResponse } from "../../../types/graph.d";
 import { Resolvers } from "../../../types/resolvers";
+import cleanNullArgs from "../../../utils/cleanNullArgs";
 import privateResolver from "../../../utils/privateResolver";
 
 const resolvers: Resolvers = {
@@ -12,17 +13,13 @@ const resolvers: Resolvers = {
                 { req }
             ): Promise<UpdateMyProfileResponse> => {
                 const user: User = req.user;
-                const notNull = {};
-                Object.keys(args).forEach(key => {
-                    if (args[key] !== null) {
-                        notNull[key] = args[key];
-                    }
-                });
+                const notNull: any = cleanNullArgs(args);
+                if (notNull.password !== null) {
+                    user.password = notNull.password;
+                    user.save();
+                    delete notNull.password;
+                }
                 try {
-                    if (args.password !== null) {
-                        user.password = args.password;
-                        user.save();
-                    }
                     await User.update({ id: user.id }, { ...notNull });
                     return {
                         ok: true,
