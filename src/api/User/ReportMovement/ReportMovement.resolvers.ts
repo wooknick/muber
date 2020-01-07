9df8626655fa8cc0adf4e83876e6e9dce4e14d8d
wooknick <wooknick@naver.com>
2020-01-07 22:57:29 +0900

    update reportMovement for subscription

--- src/api/User/ReportMovement/ReportMovement.resolvers.ts
+++ src/api/User/ReportMovement/ReportMovement.resolvers.ts
@@ -10,12 +10,15 @@ const resolvers: Resolvers = {
             async (
                 _,
                 args: ReportMovementMutationArgs,
-                { req }
+                { req, pubSub }
             ): Promise<ReportMovementResponse> => {
                 const user: User = req.user;
                 const notNull = cleanNullArgs(args);
                 try {
                     await User.update({ id: user.id }, { ...notNull });
+                    pubSub.publish("driverUpdate", {
+                        DriversSubscription: user
+                    });
                     return {
                         ok: true,
                         error: null
