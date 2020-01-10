7f5cac0b104826a19c71b6374268669a650b52d7
wooknick <wooknick@naver.com>
2020-01-11 07:02:50 +0900

    add GetChat

---
+++ src/api/Chat/GetChat/GetChat.resolvers.ts
@@ -0,0 +1,59 @@
+import Chat from "../../../entities/Chat";
+import User from "../../../entities/User";
+import { GetChatQueryArgs, GetChatResponse } from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+
+const resolvers: Resolvers = {
+    Query: {
+        GetChat: privateResolver(
+            async (
+                _,
+                args: GetChatQueryArgs,
+                { req }
+            ): Promise<GetChatResponse> => {
+                const user: User = req.user;
+                try {
+                    const chat: Chat | any = await Chat.findOne(
+                        {
+                            id: args.chatId
+                        },
+                        { relations: ["messages"] }
+                    );
+                    if (chat) {
+                        if (
+                            chat.passengerId === user.id ||
+                            chat.driverId === user.id
+                        ) {
+                            return {
+                                ok: true,
+                                error: null,
+                                chat
+                            };
+                        } else {
+                            return {
+                                ok: false,
+                                error: "not authorized to see this chat",
+                                chat: null
+                            };
+                        }
+                    } else {
+                        return {
+                            ok: false,
+                            error: "not found",
+                            chat: null
+                        };
+                    }
+                } catch (error) {
+                    return {
+                        ok: false,
+                        error: error.message,
+                        chat: null
+                    };
+                }
+            }
+        )
+    }
+};
+
+export default resolvers;
