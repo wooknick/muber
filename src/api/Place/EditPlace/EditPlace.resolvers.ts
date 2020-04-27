591fabfcda6cc00274a98cf39fea84761037b4f9
wooknick <wooknick@naver.com>
2020-04-28 02:39:27 +0900

    fix

--- src/api/Place/EditPlace/EditPlace.resolvers.ts
+++ src/api/Place/EditPlace/EditPlace.resolvers.ts
@@ -1,56 +1,53 @@
+import { EditPlaceMutationArgs, EditPlaceResponse } from "types/graph";
 import Place from "../../../entities/Place";
 import User from "../../../entities/User";
 import { Resolvers } from "../../../types/resolvers";
 import cleanNullArgs from "../../../utils/cleanNullArgs";
 import privateResolver from "../../../utils/privateResolver";
-import { EditPlaceMutationArgs, EditPlaceResponse } from "./../../../types/graph.d";
 
 const resolvers: Resolvers = {
-    Mutation: {
-        EditPlace: privateResolver(
-            async (
-                _,
-                args: EditPlaceMutationArgs,
-                { req }
-            ): Promise<EditPlaceResponse> => {
-                const user: User = req.user;
-                try {
-                    const place = await Place.findOne({ id: args.placeId });
-                    if (place) {
-                        if (place.userId === user.id) {
-                            const notNull: any = cleanNullArgs(args);
-                            if (notNull.placeId) {
-                                delete notNull.placeId;
-                            }
-                            await Place.update(
-                                { id: args.placeId },
-                                { ...notNull }
-                            );
-                            return {
-                                ok: true,
-                                error: null
-                            };
-                        } else {
-                            return {
-                                ok: false,
-                                error: "Not Authorized"
-                            };
-                        }
-                    } else {
-                        return {
-                            ok: false,
-                            error: "Place not found"
-                        };
-                    }
-                } catch (error) {
-                    return {
-                        ok: false,
-                        error: error.message
-                    };
-                }
+  Mutation: {
+    EditPlace: privateResolver(
+      async (
+        _,
+        args: EditPlaceMutationArgs,
+        { req }
+      ): Promise<EditPlaceResponse> => {
+        const user: User = req.user;
+        try {
+          const place = await Place.findOne({ id: args.placeId });
+          if (place) {
+            if (place.userId === user.id) {
+              const notNull: any = cleanNullArgs(args);
+              if (notNull.placeId) {
+                delete notNull.placeId;
+              }
+              await Place.update({ id: args.placeId }, { ...notNull });
+              return {
+                ok: true,
+                error: null,
+              };
+            } else {
+              return {
+                ok: false,
+                error: "Not Authorized",
+              };
             }
-        )
-    }
+          } else {
+            return {
+              ok: false,
+              error: "Place not found",
+            };
+          }
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+          };
+        }
+      }
+    ),
+  },
 };
 
 export default resolvers;
