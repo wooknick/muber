f0f44c6fc61d808e05df148a3d07f73f4bc6b8e6
wooknick <wooknick@naver.com>
2020-04-27 19:58:43 +0900

    minor fix

--- src/api/Ride/GetNearbyRide/GetNearbyRide.resolvers.ts
+++ src/api/Ride/GetNearbyRide/GetNearbyRide.resolvers.ts
@@ -6,51 +6,52 @@ import { Resolvers } from "../../../types/resolvers";
 import privateResolver from "../../../utils/privateResolver";
 
 const resolvers: Resolvers = {
-    Query: {
-        GetNearbyRide: privateResolver(
-            async (_, __, { req }): Promise<GetNearbyRideResponse> => {
-                const user: User = req.user;
-                if (user.isDriving) {
-                    const { lastLat, lastLng } = user;
+  Query: {
+    GetNearbyRide: privateResolver(
+      async (_, __, { req }): Promise<GetNearbyRideResponse> => {
+        const user: User = req.user;
+        if (user.isDriving) {
+          const { lastLat, lastLng } = user;
 
-                    try {
-                        const ride: Ride | any = await getRepository(
-                            Ride
-                        ).findOne({
-                            status: "REQUESTING",
-                            pickUpLat: Between(lastLat - 0.05, lastLat + 0.05),
-                            pickUpLng: Between(lastLng - 0.05, lastLng + 0.05)
-                        });
-                        if (ride) {
-                            return {
-                                ok: true,
-                                error: null,
-                                ride
-                            };
-                        } else {
-                            return {
-                                ok: true,
-                                error: null,
-                                ride: null
-                            };
-                        }
-                    } catch (error) {
-                        return {
-                            ok: false,
-                            error: error.message,
-                            ride: null
-                        };
-                    }
-                } else {
-                    return {
-                        ok: false,
-                        error: "you are not a driver",
-                        ride: null
-                    };
-                }
+          try {
+            const ride: Ride | any = await getRepository(Ride).findOne(
+              {
+                status: "REQUESTING",
+                pickUpLat: Between(lastLat - 0.05, lastLat + 0.05),
+                pickUpLng: Between(lastLng - 0.05, lastLng + 0.05),
+              },
+              { relations: ["passenger"] }
+            );
+            if (ride) {
+              return {
+                ok: true,
+                error: null,
+                ride,
+              };
+            } else {
+              return {
+                ok: true,
+                error: null,
+                ride: null,
+              };
             }
-        )
-    }
+          } catch (error) {
+            return {
+              ok: false,
+              error: error.message,
+              ride: null,
+            };
+          }
+        } else {
+          return {
+            ok: false,
+            error: "you are not a driver",
+            ride: null,
+          };
+        }
+      }
+    ),
+  },
 };
 
 export default resolvers;
--- src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
+++ src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
@@ -1,39 +1,42 @@
 import User from "../../../entities/User";
-import { UpdateMyProfileMutationArgs, UpdateMyProfileResponse } from "../../../types/graph.d";
 import { Resolvers } from "../../../types/resolvers";
 import cleanNullArgs from "../../../utils/cleanNullArgs";
 import privateResolver from "../../../utils/privateResolver";
+import {
+  UpdateMyProfileMutationArgs,
+  UpdateMyProfileResponse,
+} from "../../../types/graph.d";
 
 const resolvers: Resolvers = {
-    Mutation: {
-        UpdateMyProfile: privateResolver(
-            async (
-                _,
-                args: UpdateMyProfileMutationArgs,
-                { req }
-            ): Promise<UpdateMyProfileResponse> => {
-                const user: User = req.user;
-                const notNull: any = cleanNullArgs(args);
-                if (notNull.password !== null) {
-                    user.password = notNull.password;
-                    user.save();
-                    delete notNull.password;
-                }
-                try {
-                    await User.update({ id: user.id }, { ...notNull });
-                    return {
-                        ok: true,
-                        error: null
-                    };
-                } catch (error) {
-                    return {
-                        ok: false,
-                        error: error.message
-                    };
-                }
-            }
-        )
-    }
+  Mutation: {
+    UpdateMyProfile: privateResolver(
+      async (
+        _,
+        args: UpdateMyProfileMutationArgs,
+        { req }
+      ): Promise<UpdateMyProfileResponse> => {
+        const user: User = req.user;
+        const notNull: any = cleanNullArgs(args);
+        if (notNull.password !== null) {
+          user.password = notNull.password;
+          await user.save();
+          delete notNull.password;
+        }
+        try {
+          await User.update({ id: user.id }, { ...notNull });
+          return {
+            ok: true,
+            error: null,
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+          };
+        }
+      }
+    ),
+  },
 };
 
 export default resolvers;
