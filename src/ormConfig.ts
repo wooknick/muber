d0a0b419c863861330b7e19b69a74cb5f90935dc
wooknick <wooknick@naver.com>
2020-04-28 03:11:08 +0900

    fix db_name

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -1,15 +1,15 @@
 import { ConnectionOptions } from "typeorm";
 
 const connectionOptions: ConnectionOptions = {
-    type: "postgres",
-    database: "muber",
-    synchronize: true,
-    logging: true,
-    entities: ["entities/**/*.*"],
-    host: process.env.DB_ENDPOINT,
-    port: 5432,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD
+  type: "postgres",
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: true,
+  entities: ["entities/**/*.*"],
+  host: process.env.DB_ENDPOINT,
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
 };
 
 export default connectionOptions;
