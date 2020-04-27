1b7b7744a192e3ef754e7f79f00aa3a0e8533495
wooknick <wooknick@naver.com>
2020-04-27 23:54:10 +0900

    update relation

--- src/api/Ride/GetRide/GetRide.resolvers.ts
+++ src/api/Ride/GetRide/GetRide.resolvers.ts
@@ -5,52 +5,48 @@ import { Resolvers } from "../../../types/resolvers";
 import privateResolver from "../../../utils/privateResolver";
 
 const resolvers: Resolvers = {
-    Query: {
-        GetRide: privateResolver(
-            async (
-                _,
-                args: GetRideQueryArgs,
-                { req }
-            ): Promise<GetRideResponse> => {
-                const user: User = req.user;
-                try {
-                    const ride: Ride | any = await Ride.findOne({
-                        id: args.rideId
-                    });
-                    if (ride) {
-                        if (
-                            ride.passengerId === user.id ||
-                            ride.driverId === user.id
-                        ) {
-                            return {
-                                ok: true,
-                                error: null,
-                                ride
-                            };
-                        } else {
-                            return {
-                                ok: false,
-                                error: "not authorized",
-                                ride: null
-                            };
-                        }
-                    } else {
-                        return {
-                            ok: false,
-                            error: "ride not found",
-                            ride: null
-                        };
-                    }
-                } catch (error) {
-                    return {
-                        ok: false,
-                        error: error.message,
-                        ride: null
-                    };
-                }
+  Query: {
+    GetRide: privateResolver(
+      async (_, args: GetRideQueryArgs, { req }): Promise<GetRideResponse> => {
+        const user: User = req.user;
+        try {
+          const ride: Ride | any = await Ride.findOne(
+            {
+              id: args.rideId,
+            },
+            { relations: ["passenger", "driver"] }
+          );
+          if (ride) {
+            if (ride.passengerId === user.id || ride.driverId === user.id) {
+              return {
+                ok: true,
+                error: null,
+                ride,
+              };
+            } else {
+              return {
+                ok: false,
+                error: "not authorized",
+                ride: null,
+              };
             }
-        )
-    }
+          } else {
+            return {
+              ok: false,
+              error: "ride not found",
+              ride: null,
+            };
+          }
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+            ride: null,
+          };
+        }
+      }
+    ),
+  },
 };
 
 export default resolvers;
--- src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
+++ src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
@@ -1,78 +1,88 @@
 import Chat from "../../../entities/Chat";
 import Ride from "../../../entities/Ride";
 import User from "../../../entities/User";
-import { UpdateRideStatusMutationArgs, UpdateRideStatusResponse } from "../../../types/graph";
 import { Resolvers } from "../../../types/resolvers";
 import privateResolver from "../../../utils/privateResolver";
+import {
+  UpdateRideStatusMutationArgs,
+  UpdateRideStatusResponse,
+} from "../../../types/graph";
 
 const resolvers: Resolvers = {
-    Mutation: {
-        UpdateRideStatus: privateResolver(
-            async (
-                _,
-                args: UpdateRideStatusMutationArgs,
-                { req, pubSub }
-            ): Promise<UpdateRideStatusResponse> => {
-                const user: User = req.user;
-                if (user.isDriving) {
-                    try {
-                        let ride: Ride | undefined;
-                        if (args.status === "ACCEPTED") {
-                            ride = await Ride.findOne(
-                                {
-                                    id: args.rideId,
-                                    status: "REQUESTING"
-                                },
-                                { relations: ["passenger"] }
-                            );
-                            if (ride) {
-                                ride.driver = user;
-                                user.isTaken = true;
-                                user.save();
-                                const chat = await Chat.create({
-                                    driver: user,
-                                    passenger: ride.passenger
-                                }).save();
-                                ride.chat = chat;
-                                ride.save();
-                            }
-                        } else {
-                            ride = await Ride.findOne({
-                                id: args.rideId,
-                                driver: user
-                            });
-                        }
-                        if (ride) {
-                            ride.status = args.status;
-                            ride.save();
-                            pubSub.publish("rideUpdate", {
-                                RideStatusSubscription: ride
-                            });
-                            return {
-                                ok: true,
-                                error: null
-                            };
-                        } else {
-                            return {
-                                ok: false,
-                                error: "cant update ride"
-                            };
-                        }
-                    } catch (error) {
-                        return {
-                            ok: false,
-                            error: error.message
-                        };
-                    }
-                } else {
-                    return {
-                        ok: false,
-                        error: "you are not driving"
-                    };
-                }
+  Mutation: {
+    UpdateRideStatus: privateResolver(
+      async (
+        _,
+        args: UpdateRideStatusMutationArgs,
+        { req, pubSub }
+      ): Promise<UpdateRideStatusResponse> => {
+        const user: User = req.user;
+        if (user.isDriving) {
+          try {
+            let ride: Ride | undefined;
+            if (args.status === "ACCEPTED") {
+              ride = await Ride.findOne(
+                {
+                  id: args.rideId,
+                  status: "REQUESTING",
+                },
+                { relations: ["passenger", "driver"] }
+              );
+              if (ride) {
+                ride.driver = user;
+                user.isTaken = true;
+                user.save();
+                const chat = await Chat.create({
+                  driver: user,
+                  passenger: ride.passenger,
+                }).save();
+                ride.chat = chat;
+                ride.save();
+              }
+            } else {
+              ride = await Ride.findOne(
+                {
+                  id: args.rideId,
+                  driver: user,
+                },
+                { relations: ["passenger", "driver"] }
+              );
             }
-        )
-    }
+            if (ride) {
+              ride.status = args.status;
+              ride.save();
+              pubSub.publish("rideUpdate", {
+                RideStatusSubscription: ride,
+              });
+              return {
+                ok: true,
+                error: null,
+                rideId: ride.id,
+              };
+            } else {
+              return {
+                ok: false,
+                error: "cant update ride",
+                rideId: null,
+              };
+            }
+          } catch (error) {
+            return {
+              ok: false,
+              error: error.message,
+              rideId: null,
+            };
+          }
+        } else {
+          return {
+            ok: false,
+            error: "you are not driving",
+            rideId: null,
+          };
+        }
+      }
+    ),
+  },
 };
 
 export default resolvers;
